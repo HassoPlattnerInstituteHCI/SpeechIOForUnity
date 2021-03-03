71f74d2161a7d741862bcd61cbf439fd63398145
Corinna Jaschek <corinna.jaschek@gmail.com>
2021-03-03 15:50:57 +0100

    add keytriggered to linux speechin

--- Assets/Scripts/Speech Input Core Scripts/LinuxSpeechIn.cs
+++ Assets/Scripts/Speech Input Core Scripts/LinuxSpeechIn.cs
@@ -1,15 +1,25 @@
+using UnityEngine;
+
 public class LinuxSpeechIn : VoiceCommandBase
 {
     public LinuxSpeechIn(OnRecognized onRecognized) : base(onRecognized)
     {
+        VoiceCommandBase.onRecognized = onRecognized;
+        VoiceCommandBase.commands = new string[] { }; //default
     }
 
     public LinuxSpeechIn(OnRecognized onRecognized, string[] commands) : base(onRecognized, commands)
     {
+        VoiceCommandBase.onRecognized = onRecognized;
+        VoiceCommandBase.commands = commands;
     }
 
-    public override void KeyTriggered(string command)
+    public override void KeyTriggered(string message)
     {
+        Debug.Log("[keytrigger]:" + message);
+        VoiceCommandBase.onRecognized(message);
+        VoiceCommandBase.setRecognized(true);
+        VoiceCommandBase.recognized = message;
     }
 
     public override void PauseListening()
