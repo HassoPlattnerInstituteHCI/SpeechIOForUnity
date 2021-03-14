4004d7da6f699189f9937fdf2f72d28ccf2be126
corinnaj <corinna.jaschek@gmail.com>
2021-03-14 17:32:48 +0100

    undo changes of original sample scene

--- Assets/Scripts/Example Speech Scripts/MySpeechOutScript.cs
+++ Assets/Scripts/Example Speech Scripts/MySpeechOutScript.cs
@@ -2,39 +2,34 @@
 using System.Collections.Generic;
 using UnityEngine;
 using System.Threading.Tasks; //note that you need to include this if you want to use Task.Delay.
-using System.Threading;
 using SpeechIO;
 
 public class MySpeechOutScript : MonoBehaviour
 {
+    // Start is called before the first frame update
     SpeechOut speechOut;
-    CancellationTokenSource source;
     void Start()
     {
-        // Addind a CancellationToken is only necessary if you wish to stop speechout
-        source = new CancellationTokenSource();
-        CancellationToken token = source.Token;
         speechOut = new SpeechOut();
-        Dialog(token);
+        Dialog();
     }
 
-    public async void Dialog(CancellationToken token)
+    public async void Dialog()
     {
-        await speechOut.Speak("This is a sample speech output script.", token: token);
-        await speechOut.Speak("If you hear my voice, the speech out system is functional.", token: token);
-        await speechOut.Speak("You may hear me in a different accent. If that's the case, please check your OS speech settings.", token: token);
+        await speechOut.Speak("This is a sample speech output script.");
+        await speechOut.Speak("If you hear my voice, the speech out system is functional.");
+        await speechOut.Speak("You may hear me in a different accent. If that's the case, please check your OS speech settings.");
     }
 
+    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            source.Cancel();
-        }
+
     }
 
     void OnApplicationQuit()
     {
         speechOut.Stop(); //Windows: do not remove this line.
     }
+
 }
